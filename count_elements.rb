42dc6a42de1da901fd35a4b5f03db079cf9faf15
jchamberlain909 <jchamberlain909@gmail.com>
2018-06-04 17:43:35 -0400

    implemented count_elements method

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
 def count_elements(array)
   # code goes here
+  new_hash = {}
+  array.each do |e|
+    if new_hash.has_key?(e)
+      new_hash[e]= new_hash[e] +1
+    else
+      new_hash[e] = 1    
+    end
+  end 
+  new_hash
 end
  
